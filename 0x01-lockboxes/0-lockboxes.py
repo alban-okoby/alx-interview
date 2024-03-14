cdd1e3568dd59d5a28a13e37c10da864d23049bb
alban-okoby <albanokoby225@gmail.com>
2024-03-14 00:05:35 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
