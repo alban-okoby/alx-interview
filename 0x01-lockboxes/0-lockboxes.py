d3fdf39a4533d80e92a0d60c64a7561f88a1d2d9
alban-okoby <albanokoby225@gmail.com>
2024-03-14 00:17:10 +0000

    Issue Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,8 +6,8 @@ def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked
     """
+
     keys = [0]
-    
     for key in keys:
         for boxKey in boxes[key]:
             if boxKey not in keys and boxKey < len(boxes):
