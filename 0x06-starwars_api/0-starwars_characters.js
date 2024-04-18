13234cb59a84e9b0a4305c8de203d84a7a174243
alban-okoby <albanokoby225@gmail.com>
2024-04-18 23:06:46 +0000

    Issue Stars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,65 @@
 #!/usr/bin/node
 
-// Use Axios
-const axios = require('axios');
+const request = require('request');
+const process = require('process');
 
-const mvId = process.argv[2];
-
-async function getCharacters(mvId) {
-  try {
-    const url = `https://swapi-api.hbtn.io/api/films/${mvId}`;
-    const response = await axios.get(url);
-    const charactersArray = response.data.characters;
-    
-    for (const character of charactersArray) {
-      const characterResponse = await axios.get(character);
-      console.log(characterResponse.data.name);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+function getMovieCharacters(movieId) {
+    const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+    return new Promise((resolve, reject) => {
+        request.get(filmUrl, (error, response, body) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (response.statusCode !== 200) {
+                reject(`Failed to fetch movie data. Status code: ${response.statusCode}`);
+                return;
+            }
+            const filmData = JSON.parse(body);
+            const charactersUrls = filmData.characters;
+            const characterRequests = charactersUrls.map(url => {
+                return new Promise((resolve, reject) => {
+                    request.get(url, (error, response, body) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+                        if (response.statusCode !== 200) {
+                            reject(`Failed to fetch character data. Status code: ${response.statusCode}`);
+                            return;
+                        }
+                        const characterData = JSON.parse(body);
+                        resolve(characterData.name);
+                    });
+                });
+            });
+            Promise.all(characterRequests)
+                .then(characters => resolve(characters))
+                .catch(error => reject(error));
+        });
+    });
 }
 
-getCharacters(mvId);
+function main() {
+    const movieId = process.argv[2];
+    if (!movieId) {
+        console.log("Usage: node script.js <Movie_ID>");
+        return;
+    }
+
+    getMovieCharacters(movieId)
+        .then(characters => {
+            if (characters.length > 0) {
+                characters.forEach(character => {
+                    console.log(character);
+                });
+            } else {
+                console.log("No characters found for the given movie ID.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
+
+main();
