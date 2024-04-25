6063b552d571abd65add67738a173bfcca1e92da
alban-okoby <albanokoby225@gmail.com>
2024-04-25 19:28:28 +0000

    Fixe code style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,9 +8,9 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
     for i in range(n):
         matrix[i].reverse()
+
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
