119b1d88efa311b7fec1de79d07a5b4ca18a74a0
alban-okoby <albanokoby225@gmail.com>
2024-05-01 18:00:13 +0000

    Fixe codestyle issue

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,17 +4,20 @@
 Main program
 """
 
-
 def makeChange(coins, total):
-    if total < 0:
+    """Calculate the fewest number of coins needed to meet the given total amount"""
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    num_coins = 0
+    for coin in coins:
+        while total >= coin:
+            num_coins += 1
+            total -= coin
+
+    if total == 0:
+        return num_coins
+    else:
         return -1
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if amount >= coin:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
