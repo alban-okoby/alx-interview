fc4ec2dc5667184dc07923355c901f59b2754eba
alban-okoby <albanokoby225@gmail.com>
2024-05-08 11:00:10 +0000

    Fix issue of pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,17 +9,16 @@ def island_perimeter(grid):
 
     rows, cols = len(grid), len(grid[0])
     perimeter = 0
-
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    
-                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols or grid[nr][nc] == 0:
+                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                        perimeter += 1
+                    elif grid[nr][nc] == 0:
                         perimeter += 1
 
     return perimeter
