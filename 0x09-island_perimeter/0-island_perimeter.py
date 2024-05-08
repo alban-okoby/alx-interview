f09526e4c7fef1bc39c57115df1e0d9e7f2cf9ea
alban-okoby <albanokoby225@gmail.com>
2024-05-08 10:51:01 +0000

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" Island Perimeter  """
+
+
+def island_perimeter(grid):
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    
+                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols or grid[nr][nc] == 0:
+                        perimeter += 1
+
+    return perimeter
