08119b3db310e4f135098ef46c177ea826affabb
alban-okoby <albanokoby225@gmail.com>
2024-05-14 22:30:04 +0000

    Another style

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,38 +8,52 @@ def isWinner(x, nums):
         is_prime = [True] * (max_num + 1)
         p = 2
         while (p * p <= max_num):
-            if (is_prime[p] == True):
+            if (is_prime[p]):
                 for i in range(p * p, max_num + 1, p):
                     is_prime[i] = False
             p += 1
-        is_prime[0], is_prime[1] = False, False
-        return is_prime
+        prime_numbers = [p for p in range(2, max_num + 1) if is_prime[p]]
+        return prime_numbers
 
-    def simulate_game(n):
-        is_prime = sieve_of_eratosthenes(n)
-        p_nbs = [i for i in range(2, n + 1) if is_prime[i]]
+    max_n = max(nums)
+    prime_numbers = sieve_of_eratosthenes(max_n)
 
-        player_turn = 0
-        while p_nbs:
-            selected_prime = p_nbs[0]
-            p_nbs = [num for num in p_nbs if num % selected_prime != 0]
-            player_turn = 1 - player_turn
+    def play_game(n, primes):
+        remaining_numbers = set(range(1, n + 1))
+        current_player = 0
 
-        return player_turn
+        while True:
+            can_move = False
+            for prime in primes:
+                if prime in remaining_numbers:
+                    can_move = True
+                    multiples = set(range(prime, n + 1, prime))
+                    remaining_numbers -= multiples
+
+                    if not remaining_numbers:
+                        return current_player
+
+                    break
+            if not can_move:
+                return 1 - current_player
+
+            current_player = 1 - current_player
 
     maria_wins = 0
     ben_wins = 0
 
-    for n in nums:
-        winner = simulate_game(n)
+    for i in range(x):
+        n = nums[i]
+        primes_up_to_n = [p for p in prime_numbers if p <= n]
+        winner = play_game(n, primes_up_to_n)
         if winner == 0:
             maria_wins += 1
         else:
             ben_wins += 1
 
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
+        return 'Ben'
     else:
         return None
