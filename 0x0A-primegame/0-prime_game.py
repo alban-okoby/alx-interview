e470a9a182992e83e92a309da9f571ec51955690
alban-okoby <albanokoby225@gmail.com>
2024-05-14 22:40:37 +0000

    Try another one

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,54 +3,46 @@
 """ Prime Game """
 
 
+def prime_numbers_up_to(n):
+    """ List of prime numbers up to 'n' using the Sieve of Eratosthenes """
+    is_prime = [True] * (n + 1)
+    p = 2
+    while p * p <= n:
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if is_prime[p]]
+
+
 def isWinner(x, nums):
-    def sieve_of_eratosthenes(max_num):
-        is_prime = [True] * (max_num + 1)
-        p = 2
-        while (p * p <= max_num):
-            if (is_prime[p]):
-                for i in range(p * p, max_num + 1, p):
-                    is_prime[i] = False
-            p += 1
-        prime_numbers = [p for p in range(2, max_num + 1) if is_prime[p]]
-        return prime_numbers
-
-    max_n = max(nums)
-    prime_numbers = sieve_of_eratosthenes(max_n)
-
-    def play_game(n, primes):
-        remaining_numbers = set(range(1, n + 1))
-        current_player = 0
-
-        while True:
-            can_move = False
-            for prime in primes:
-                if prime in remaining_numbers:
-                    can_move = True
-                    multiples = set(range(prime, n + 1, prime))
-                    remaining_numbers -= multiples
-
-                    if not remaining_numbers:
-                        return current_player
-
-                    break
-            if not can_move:
-                return 1 - current_player
-
-            current_player = 1 - current_player
-
+    """ Winner of the Prime Game for given rounds and upper limits """
+    if x is None or nums is None or x == 0 or not nums:
+        return None
     maria_wins = 0
     ben_wins = 0
-
-    for i in range(x):
-        n = nums[i]
-        primes_up_to_n = [p for p in prime_numbers if p <= n]
-        winner = play_game(n, primes_up_to_n)
-        if winner == 0:
+    for n in nums:
+        primes = prime_numbers_up_to(n)
+        remaining_numbers = set(range(1, n + 1))
+        current_player = 'Maria'
+        while remaining_numbers:
+            playable_primes = [p for p in primes if p in remaining_numbers]
+            if not playable_primes:
+                break
+            # Who is the winner of this turn ?
+            if current_player == 'Maria':
+                current_player = 'Ben'
+            else:
+                current_player = 'Maria'
+            # smallest playable prime and remove its multiples
+            chosen_prime = min(playable_primes)
+            for multiple in range(chosen_prime, n + 1, chosen_prime):
+                if multiple in remaining_numbers:
+                    remaining_numbers.remove(multiple)
+        if current_player == 'Maria':
             maria_wins += 1
         else:
             ben_wins += 1
-
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
