415a1d92f62932b06b249c021a12c26a26d6d0c9
alban-okoby <albanokoby225@gmail.com>
2024-05-14 22:18:45 +0000

    Output state

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,44 +3,43 @@
 """ Prime Game """
 
 
-def prime_numbers(n):
-    """Return list of prime numbers between 1 and n inclusive
-    Args:
-        n (int): upper boundary of range. The lower boundary is always 1
-    """
-    prime_nos = []
-    filtered = [True] * (n + 1)
-    for prime in range(2, n + 1):
-        if filtered[prime]:
-            prime_nos.append(prime)
-            for i in range(prime * prime, n + 1, prime):
-                filtered[i] = False
-    return prime_nos
-
-
 def isWinner(x, nums):
-    """
-    Determines winner of Prime Game
-    Args:
-        x (int): number of rounds of the game
-        nums (list): list of upper limits of range for each round
-    Returns:
-        Name of the winner (Maria or Ben) or None
-    """
-    if x is None or nums is None or x == 0 or not nums:
-        return None
+    def sieve_of_eratosthenes(max_num):
+        is_prime = [True] * (max_num + 1)
+        p = 2
+        while (p * p <= max_num):
+            if (is_prime[p] == True):
+                for i in range(p * p, max_num + 1, p):
+                    is_prime[i] = False
+            p += 1
+        is_prime[0], is_prime[1] = False, False
+        return is_prime
 
-    maria = ben = 0
-    for num in nums:
-        prime_numbers_list = prime_numbers(num)
-        if len(prime_numbers_list) % 2 == 0:
-            ben += 1
+    def simulate_game(n):
+        is_prime = sieve_of_eratosthenes(n)
+        p_nbs = [i for i in range(2, n + 1) if is_prime[i]]
+
+        player_turn = 0
+        while p_nbs:
+            selected_prime = p_nbs[0]
+            p_nbs = [num for num in p_nbs if num % selected_prime != 0]
+            player_turn = 1 - player_turn
+
+        return player_turn
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        winner = simulate_game(n)
+        if winner == 0:
+            maria_wins += 1
         else:
-            maria += 1
+            ben_wins += 1
 
-    if maria > ben:
-        return 'Maria'
-    elif ben > maria:
-        return 'Ben'
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
